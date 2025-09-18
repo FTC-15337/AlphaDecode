25244be05cb3415d12033385abecbc17754ddab3
Ranveer1010 <ranveer.nagar2011@gmail.com>
2025-09-18 16:25:13 -0700

    Changed to linear op mode and added x printing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearningJava/PrintingOnRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearningJava/PrintingOnRobot.java
@@ -1,17 +1,21 @@
 package org.firstinspires.ftc.teamcode.LearningJava;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class PrintingOnRobot extends OpMode {
-    @Override
-    public void init(){
-        telemetry.addLine("Hello World");
+public class PrintingOnRobot extends LinearOpMode {
+    public void initialize() {
+        telemetry.addLine("Init");
     }
     @Override
-    public void loop(){
-       telemetry.addData("Joystick value is : ", gamepad1.left_stick_y);
+    public void runOpMode(){
+        initialize();
+        waitForStart();
+        while(!isStopRequested() && opModeIsActive()){
+            telemetry.addData("Gamepad 1 stick y is " , gamepad1.left_stick_y);
+            telemetry.addData("Gamepad 1 stick x is " , gamepad1.left_stick_x);
+        }
     }
-
 }
