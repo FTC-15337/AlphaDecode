7342d7e158c090b9462cfc86ee48976875e00641
Gahan Mummidi <gahanmummidi@gmail.com>
2025-09-18 17:08:11 -0700

    Added right stick values for x and y also added initialized telemetry and gamepad.a telemetry line

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearningJava/PrintingOnRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearningJava/PrintingOnRobot.java
@@ -6,16 +6,27 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class PrintingOnRobot extends LinearOpMode {
-    public void initialize() {
-        telemetry.addLine("Init");
-    }
     @Override
-    public void runOpMode(){
-        initialize();
-        waitForStart();
-        while(!isStopRequested() && opModeIsActive()){
-            telemetry.addData("Gamepad 1 stick y is " , gamepad1.left_stick_y);
-            telemetry.addData("Gamepad 1 stick x is " , gamepad1.left_stick_x);
+    public void runOpMode() throws InterruptedException {
+
+        init();
+        while (opModeInInit()) {
+            telemetry.addLine("Initialized");
+            telemetry.update();
+        }
+
+        start();
+        while (opModeIsActive()) {
+            telemetry.addData("Gamepad 1 stick y is ", gamepad1.left_stick_y);
+            telemetry.addData("Gamepad 1 stick x is ", gamepad1.left_stick_x);
+            telemetry.addData("Gamepad 1 stick y is ", gamepad1.right_stick_y);
+            telemetry.addData("Gamepad  1 stick x is ", gamepad1.right_stick_x);
+
+            if (gamepad1.a){
+                telemetry.addLine("Robotics is fun!!!");
+            }
+
+            telemetry.update();
         }
     }
 }
