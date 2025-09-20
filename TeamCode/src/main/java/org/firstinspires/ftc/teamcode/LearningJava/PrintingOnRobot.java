5644aab4e870e511425b12a10494dc27ebb4adbd
Gahan Mummidi <gahanmummidi@gmail.com>
2025-09-19 20:44:54 -0700

    Added joystick code to move robot forward backword left and right for left joystick only

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearningJava/PrintingOnRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearningJava/PrintingOnRobot.java
@@ -1,31 +1,89 @@
 package org.firstinspires.ftc.teamcode.LearningJava;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 
-@TeleOp
+@TeleOp(name = "Joystick Program")
 public class PrintingOnRobot extends LinearOpMode {
+
+    private DcMotor frontLeft, frontRight, backLeft, backRight;
     @Override
     public void runOpMode() throws InterruptedException {
 
         init();
         while (opModeInInit()) {
-            telemetry.addLine("Initialized");
+            telemetry.addLine("Everything is initialized!");
             telemetry.update();
         }
 
-        start();
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+
+        waitForStart();
         while (opModeIsActive()) {
             telemetry.addData("Gamepad 1 stick y is ", gamepad1.left_stick_y);
             telemetry.addData("Gamepad 1 stick x is ", gamepad1.left_stick_x);
             telemetry.addData("Gamepad 1 stick y is ", gamepad1.right_stick_y);
-            telemetry.addData("Gamepad  1 stick x is ", gamepad1.right_stick_x);
+            telemetry.addData("Gamepad 1 stick x is ", gamepad1.right_stick_x);
 
             if (gamepad1.a){
                 telemetry.addLine("Robotics is fun!!!");
             }
-
+            if (gamepad1.left_stick_y < -0.5) {
+                frontLeft.setPower(-0.5);
+                frontRight.setPower(-0.5);
+                backLeft.setPower(0.5);
+                backRight.setPower(-0.5);
+            }
+            if (gamepad1.left_stick_y > 0.5) {
+                frontLeft.setPower(0.5);
+                frontRight.setPower(0.5);
+                backLeft.setPower(-0.5);
+                backRight.setPower(0.5);
+            }
+            if (gamepad1.left_stick_y < 0.5) {
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backLeft.setPower(0);
+                backRight.setPower(0);
+            }
+            if (gamepad1.left_stick_y > -0.5) {
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backLeft.setPower(0);
+                backRight.setPower(0);
+            }
+            if (gamepad1.left_stick_x < -0.5) {
+                frontLeft.setPower(1);
+                frontRight.setPower(0);
+                backLeft.setPower(1);
+                backRight.setPower(0);
+            }
+            if (gamepad1.left_stick_x > 0.5) {
+                frontLeft.setPower(0);
+                frontRight.setPower(1);
+                backLeft.setPower(0);
+                backRight.setPower(-1);
+            }
+            if (gamepad1.left_stick_x < 0.5) {
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backLeft.setPower(0);
+                backRight.setPower(0);
+            }
+            if (gamepad1.left_stick_x < -0.5) {
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backLeft.setPower(0);
+                backRight.setPower(0);
+            }
             telemetry.update();
         }
     }
