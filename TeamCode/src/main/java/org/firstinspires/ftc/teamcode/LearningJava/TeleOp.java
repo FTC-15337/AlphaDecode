a5c50a043964e3bdacd71c78216b393dbd2fd326
Gahan Mummidi <gahanmummidi@gmail.com>
2025-09-23 16:08:22 -0700

    Renamed TelOp file and only has mec drivebase code not pinpoint or april tag detection.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearningJava/TeleOp.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.LearningJava;
+
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "MecBase")
+public class TeleOp extends LinearOpMode {
+
+    MecDrivebase drive = new MecDrivebase();
+    GoBildaPinpointDriver pinpoint;
+    double forward, strafe, rotate;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drive.init(hardwareMap);
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        telemetry.addLine("Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while(!isStopRequested() && opModeIsActive()) {
+            telemetry.addData("Left Joystick Y ", gamepad1.left_stick_y);
+            telemetry.addData("Left Joystick X", gamepad1.left_stick_x);
+            forward = gamepad1.left_stick_y;
+            strafe = -gamepad1.left_stick_x;
+            rotate = gamepad1.right_stick_x;
+
+            drive.driveFieldRelative(forward, strafe, rotate);
+
+            telemetry.update();
+        }
+    }
+}
