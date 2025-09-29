b6bbc8441d865519c4612281c7cbd2ef1a2227d5
Ranveer1010 <ranveer.nagar2011@gmail.com>
2025-09-28 19:23:50 -0700

    Removed unused constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConstantValues/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConstantValues/Constants.java
@@ -6,7 +6,4 @@ public class Constants {
     public static double testMotorPower = 0.5;
     public static double testMotorStop = 0.0;
 
-    public static final int Max_BALL_STORAGE = 3;
-
-    public static final double[] SLOT_SERVO_POSITIONS = {0.2, 0.5, 0.8};
 }
