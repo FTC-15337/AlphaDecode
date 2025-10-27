ee573fcd3806232283e16bdf205b90073684f705
Gahan Mummidi <gahanmummidi@gmail.com>
2025-10-26 18:06:11 -0700

    Added pinpoint code to drive strafe and turn.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/LimeLightConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/LimeLightConfig.java
@@ -4,29 +4,10 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
 public class LimeLightConfig {
-
-    private Limelight3A limelight;
-
-    public void init(HardwareMap hwMap) {
-        limelight = hwMap.get(Limelight3A.class, "limelight");
-        limelight.pipelineSwitch(11);
-        limelight.start();
-    }
-
-    public boolean hasTarget() {
-        LLResult result = limelight.getLatestResult();
-        return result != null && result.isValid();
-    }
-
-    public double getTx() {
-        LLResult result = limelight.getLatestResult();
-        return (result != null && result.isValid()) ? result.getTx() : 0;
-    }
-
-    public double getTa() {
-        LLResult result = limelight.getLatestResult();
-        return (result != null && result.isValid()) ? result.getTa() : 0;
-    }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto.java
@@ -1,72 +1,40 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-import org.firstinspires.ftc.teamcode.Mechanisms.LimeLightConfig;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import static org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit.CM;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.Mechanisms.LimeLightConfig;
 import org.firstinspires.ftc.teamcode.Mechanisms.MecDrivebase;
-import org.firstinspires.ftc.teamcode.Mechanisms.ShooterConfig;
+import org.firstinspires.ftc.teamcode.Mechanisms.Pinpoint;
 import org.firstinspires.ftc.teamcode.Mechanisms.ServoKick;
-import org.firstinspires.ftc.teamcode.Mechanisms.SortingWithColor;
-import org.firstinspires.ftc.teamcode.Mechanisms.StorageConfig;
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Auto")
-public class Auto extends LinearOpMode {
 
-    private MecDrivebase mecDrivebase = new MecDrivebase();
-    private LimeLightConfig ll = new LimeLightConfig();
-    private ShooterConfig shooter = new ShooterConfig();
+@Autonomous(name = "Autonomous")
+public class Auto extends LinearOpMode
+{
+    // get an instance of the "Robot" class.
+    private Pinpoint robot = new Pinpoint(this);
+    private MecDrivebase drive = new MecDrivebase();
     private ServoKick kick = new ServoKick();
-    private StorageConfig sorter = new StorageConfig();
-    private SortingWithColor colorSensor = new SortingWithColor();
 
-    @Override
-    public void runOpMode() throws InterruptedException {
+    private Limelight3A limelight;
 
-        mecDrivebase.init(hardwareMap);
-        ll.init(hardwareMap);
-        shooter.init(hardwareMap);
-        kick.init(hardwareMap);
-        sorter.init(hardwareMap);
-        colorSensor.init(hardwareMap);
+    @Override public void runOpMode() throws InterruptedException {
+        // Initialize the robot hardware & Turn on telemetry
+        robot.init(true);
 
-        shooter.stopMotor();
-        kick.retract();
-
-        telemetry.addLine("Initialized");
-        telemetry.update();
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-            autoAlignToTag();
-            if (ll.getTa() >= 3.8) {
-                mecDrivebase.drive(0, 0, 0);
-                telemetry.addLine("Aligned to tag");
-                telemetry.update();
-                break;
-            }
-        }
-    }
-
-    public void autoAlignToTag() {
-        if (!ll.hasTarget()) {
-            mecDrivebase.drive(0, 0, 0);
-            telemetry.addLine("No Tag Detected");
-            telemetry.update();
-            return;
-        }
-        double tx = ll.getTx();
-        double ta = ll.getTa();
-        double strafe = -tx * 0.03;
-        double forward = (3.8 - ta) * 0.4;
-        double rotate = 0;
-        mecDrivebase.drive(forward, strafe, rotate);
-
-        telemetry.addData("tx", tx);
-        telemetry.addData("ta", ta);
-        telemetry.addData("Forward", forward);
-        telemetry.addData("Strafe", strafe);
-        telemetry.update();
+        robot.drive(12, 1.0, 1.0);
     }
 }
